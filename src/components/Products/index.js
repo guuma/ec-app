56b0f6c969b47961e0f216f75c8b386722c57b20
masashi-yamashita <rabbit.wonderfuleveryday@gmail.com>
2020-09-29 02:12:13 +0900

    Reanme  ProductCard.jsx

--- src/components/Products/index.js
+++ src/components/Products/index.js
@@ -3,4 +3,4 @@ export { default as ImagePreview } from './ImagePreview';
 export { default as ImageSwiper } from './ImageSwiper';
 export { default as SetSizeArea } from './SetSizeArea';
 export { default as SizeTable } from './SizeTable';
-export { default as ProductCard } from './ProducyCard';
+export { default as ProductCard } from './ProductCard';
