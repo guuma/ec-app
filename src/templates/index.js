385fcc1cae5b102515321654b7c2eb133af44e5e
masashi-yamashita <rabbit.wonderfuleveryday@gmail.com>
2020-10-03 00:12:03 +0900

    Add order function

--- src/templates/index.js
+++ src/templates/index.js
@@ -6,3 +6,4 @@ export { default as SignUp } from './SignUp';
 export { default as SingIn } from './SignIn';
 export { default as ResetPassword } from './ResetPassword';
 export { default as CartList } from './CartList';
+export { default as OrderConfirm } from './OrderConfirm';
