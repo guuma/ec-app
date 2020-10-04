e3356b6204d379460e8c2a7496ee7d92edb2ec2e
masashi-yamashita <rabbit.wonderfuleveryday@gmail.com>
2020-10-05 03:07:32 +0900

    Fix ProductList.jsx

--- src/templates/ProductList.jsx
+++ src/templates/ProductList.jsx
@@ -9,7 +9,7 @@ const ProductList = () => {
   const products = getProducts(selector);
   const query = selector.router.location.search;
   const gender = /^\?gender=/.test(query) ? query.split('?gender=')[1] : '';
-  const category = /^\?category=/.test(query) ? query.split('?category')[1] : '';
+  const category = /^\?category=/.test(query) ? query.split('?category=')[1] : '';
   useEffect(() => {
     dispatch(fetchProducts(gender, category));
   }, [query]);
