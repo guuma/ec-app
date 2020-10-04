2a2d8519943609604988f990ae3061f2d12bcac8
masashi-yamashita <rabbit.wonderfuleveryday@gmail.com>
2020-10-05 02:02:22 +0900

    Install redux-logger

--- src/reducks/store/store.js
+++ src/reducks/store/store.js
@@ -1,17 +1,27 @@
 import { createStore as reduxCreateStore, combineReducers, applyMiddleware } from 'redux';
 import { connectRouter, routerMiddleware } from 'connected-react-router';
 import thunk from 'redux-thunk';
+import { createLogger } from 'redux-logger';
 // Import reducers
 import { UsersReducer } from '../users/reducers';
 import { ProductsReducer } from '../products/reducers';
 
 export default function createStore(history) {
+  const middlewares = [routerMiddleware(history), thunk];
+
+  if (process.env.NODE_ENV === 'development') {
+    const logger = createLogger({
+      collapsed: true,
+      diff: true,
+    });
+    middlewares.push(logger);
+  }
   return reduxCreateStore(
     combineReducers({
       router: connectRouter(history),
       users: UsersReducer,
       products: ProductsReducer,
     }),
-    applyMiddleware(routerMiddleware(history), thunk)
+    applyMiddleware(...middlewares)
   );
 }
